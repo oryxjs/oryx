90befe49eaa98ad5f63ab077653de7c8578afa22
Saleel S <saleel97@gmail.com>
2019-06-01 01:01:11 +0400

    Implement logger methods in application

--- test/application.test.js
+++ test/application.test.js
@@ -9,4 +9,27 @@ describe('Oryx application', () => {
   test('should initialize', async () => {
     expect(this.app).toBeTruthy();
   });
+
+
+  describe('Logger', () => {
+    test('should implement logger methods', async () => {
+      expect(typeof this.app.log).toBe('function');
+      expect(typeof this.app.info).toBe('function');
+      expect(typeof this.app.debug).toBe('function');
+      expect(typeof this.app.error).toBe('function');
+      expect(typeof this.app.warn).toBe('function');
+    });
+
+    test('should use the custom logger if passed', async () => {
+      const customLogger = {
+        log: jest.fn(),
+      };
+
+      const appWithLogger = new Oryx({ logger: customLogger });
+      expect(appWithLogger.log).toBe(customLogger.log);
+
+      appWithLogger.log('custom logger works');
+      expect(customLogger.log).toBeCalledWith('custom logger works');
+    });
+  });
 });
