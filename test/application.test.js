d37a75075259a9a971ea3ee8f00ae6708e05bd87
Saleel S <saleel97@gmail.com>
2019-06-01 14:10:14 +0400

    Add service related methods to application

--- test/application.test.js
+++ test/application.test.js
@@ -3,7 +3,7 @@ const Oryx = require('../lib');
 
 
 describe('Oryx application', () => {
-  beforeAll(async () => {
+  beforeEach(async () => {
     this.app = new Oryx();
   });
 
@@ -38,6 +38,52 @@ describe('Oryx application', () => {
   describe('Event Emitter', () => {
     test('should be an instance of EventEmitter', async () => {
       expect(this.app).toBeInstanceOf(EventEmitter);
+    });
+  });
+
+
+  describe('Service', () => {
+    test('should load service', async () => {
+      class TestService extends Oryx.Service {
+        start() { }
+      }
+
+      this.app.load('test', new TestService());
+      expect(this.app.service('test')).toBeInstanceOf(TestService);
+    });
+
+    test('should throw an error if service name already exists', async () => {
+      expect(() => {
+        this.app.load('test', new Oryx.Service());
+        this.app.load('test', new Oryx.Service());
+      }).toThrow('A service with name "test" is already loaded.');
+    });
+
+    test('should throw an error if not an instance of Oryx.Service', async () => {
+      expect(() => {
+        this.app.load('test', {});
+      }).toThrow('Service "test" is not an instance of Oryx.Service.');
+    });
+
+    test('should return the created service', async () => {
+      class TestService extends Oryx.Service {
+        start() { }
+      }
+      const testService = new TestService();
+
+      const loadedService = this.app.load('test', testService);
+      expect(loadedService).toBe(testService);
+    });
+
+    test('should return service on calling app.service()', async () => {
+      class TestService extends Oryx.Service {
+        start() { }
+      }
+      const testService = new TestService();
+
+      this.app.load('test', testService);
+
+      expect(this.app.service('test')).toBe(testService);
     });
   });
 });
