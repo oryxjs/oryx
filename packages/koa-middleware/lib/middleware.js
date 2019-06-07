1c22be2c3ebca7a099628e5e36aa881ad3dc4052
Saleel S <saleel97@gmail.com>
2019-06-07 19:26:28 +0400

    Implement route handling

--- packages/koa-middleware/lib/middleware.js
+++ packages/koa-middleware/lib/middleware.js
@@ -1,11 +1,79 @@
+const Router = require('koa-tree-router');
+
+
+function getServiceMethod(httpMethod, isMany) {
+  switch (httpMethod) {
+    case 'GET': {
+      return isMany ? 'getMany' : 'get';
+    }
+    case 'POST': {
+      return 'post';
+    }
+    case 'PUT': {
+      return isMany ? 'putMany' : 'put';
+    }
+    case 'PATCH': {
+      return isMany ? 'patchMany' : 'patch';
+    }
+    case 'DELETE': {
+      return isMany ? 'deleteMany' : 'delete';
+    }
+
+    default: {
+      return null;
+    }
+  }
+}
+
+
 function createMiddleware(app) {
   if (!app || typeof app.start !== 'function') {
-    throw new Error('Parameter should be an oryx app. Got {app}');
+    throw new Error(`Parameter should be an oryx app. Got ${app}`);
   }
 
-  return (req, res, next) => {
-    next();
-  };
+  const router = new Router();
+
+  // Register routes
+  for (const serviceName of Object.keys(app.services)) {
+    const path = `/${serviceName}`;
+    const idPath = `/${serviceName}/:id`;
+    const service = app.services[serviceName];
+
+    router.all(path, async (ctx) => {
+      const { method: httpMethod } = ctx;
+      const serviceMethod = getServiceMethod(httpMethod, true);
+
+      const params = {
+        query: ctx.query,
+        route: ctx.params,
+        body: ctx.request.body,
+      };
+
+      ctx.body = await service[serviceMethod](params);
+    });
+
+    router.all(idPath, async (ctx) => {
+      const { method: httpMethod } = ctx;
+      const serviceMethod = getServiceMethod(httpMethod, false);
+
+      const params = {
+        query: ctx.query,
+        route: ctx.params,
+        body: ctx.request.body,
+      };
+
+      const result = await service[serviceMethod](ctx.params.id, params);
+      ctx.body = result;
+
+      if (typeof result === 'undefined') {
+        if (httpMethod === 'GET') {
+          ctx.status = 404;
+        }
+      }
+    });
+  }
+
+  return router.routes();
 }
 
 
--- packages/koa-middleware/test/middleware.test.js
+++ packages/koa-middleware/test/middleware.test.js
@@ -1,15 +1,139 @@
+const Koa = require('koa');
+const axios = require('axios');
 const Oryx = require('@oryx/oryx');
+const bodyParser = require('koa-bodyparser');
 const createMiddleware = require('../');
 
 
 describe('Oryx application', () => {
-  beforeEach(async () => {
-    this.app = new Oryx();
+  test('should accept oryx and return middleware', () => {
+    const middleware = createMiddleware(new Oryx());
+    expect(typeof middleware).toBe('function');
   });
 
-  test('should accept oryx and return middleware', () => {
-    const middleware = createMiddleware(this.app);
+  test('should throw if the paramter is not an oryx app', () => {
+    expect(() => {
+      createMiddleware('bad');
+    }).toThrow('Parameter should be an oryx app. Got bad');
+  });
 
-    expect(typeof middleware).toBe('function');
+
+  describe('Routing', () => {
+    beforeAll(async () => {
+      class ConfigService extends Oryx.Service {
+        async get(id) {
+          return this.config.find(c => c.id === id);
+        }
+
+        async getMany() {
+          return this.config;
+        }
+
+        async post({ body = {} }) {
+          const { id, value, label } = body;
+          this.config.push({ id, value, label });
+
+          return this.config.find(c => c.id === id);
+        }
+
+        async put(id, { body = {} }) {
+          const { value, text } = body;
+          const config = this.config.find(c => c.id === id);
+          config.value = value;
+          config.text = text;
+
+          return this.config.find(c => c.id === id);
+        }
+
+        async patch(id, { body = {} }) {
+          const { value, text } = body;
+          const config = this.config.find(c => c.id === id);
+          if (typeof value !== 'undefined') {
+            config.value = value;
+          }
+          if (typeof text !== 'undefined') {
+            config.text = text;
+          }
+
+          return this.config.find(c => c.id === id);
+        }
+
+        async delete(id) {
+          this.config = this.config.filter(c => c.id !== id);
+        }
+      }
+
+      this.app = new Oryx();
+      this.configService = new ConfigService();
+      this.app.load('config', this.configService);
+
+      const koa = new Koa();
+      const oryxMiddleware = createMiddleware(this.app);
+      koa.use(bodyParser({ enableTypes: ['json'] }));
+      koa.use(oryxMiddleware);
+      this.server = koa.listen(9779);
+
+      this.axiosInstance = axios.create({
+        baseURL: 'http://localhost:9779/',
+        timeout: 1000,
+        headers: { 'X-Custom-Header': 'foobar' },
+      });
+    });
+
+    beforeEach(() => {
+      this.configService.config = [{
+        id: 'one',
+        value: 1,
+        text: 'One',
+      }];
+    });
+
+    afterAll(() => {
+      this.server.close();
+    });
+
+    test('should route GET request to get service method', async () => {
+      const { data } = await this.axiosInstance.get('/config');
+      expect(data).toEqual([{ id: 'one', value: 1, text: 'One' }]);
+    });
+
+    test('should route GET /:id request to getOne service method', async () => {
+      const { data } = await this.axiosInstance.get('/config/one');
+      expect(data).toEqual({ id: 'one', value: 1, text: 'One' });
+    });
+
+    test('should route POST request to post service method', async () => {
+      const { data } = await this.axiosInstance.post('/config', { id: 'two', value: 2, label: 'Two' });
+      expect(data).toEqual({ id: 'two', value: 2, label: 'Two' });
+
+      const { data: getData } = await this.axiosInstance.get('/config/two');
+      expect(getData).toEqual({ id: 'two', value: 2, label: 'Two' });
+    });
+
+    test('should route PUT request to put service method', async () => {
+      const { data } = await this.axiosInstance.put('/config/one', { value: -1, text: 'Minus One' });
+      expect(data).toEqual({ id: 'one', value: -1, text: 'Minus One' });
+
+      const { data: getData } = await this.axiosInstance.get('/config/one');
+      expect(getData).toEqual({ id: 'one', value: -1, text: 'Minus One' });
+    });
+
+    test('should route PATCH request to patch service method', async () => {
+      const { data } = await this.axiosInstance.patch('/config/one', { value: 11 });
+      expect(data).toEqual({ id: 'one', value: 11, text: 'One' });
+
+      const { data: getData } = await this.axiosInstance.get('/config/one');
+      expect(getData).toEqual({ id: 'one', value: 11, text: 'One' });
+    });
+
+    test('should route DELETE request to delete service method', async () => {
+      const result = await this.axiosInstance.delete('/config/one');
+      expect(result.status).toEqual(204);
+
+      try {
+        const getResult = await this.axiosInstance.get('/config/one');
+        expect(getResult.status).toEqual(404);
+      } catch (e) {}
+    });
   });
 });
